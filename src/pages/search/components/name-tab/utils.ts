5a40ff4b4cc77c22cb2460cc9a1b22ba6450febf
BobesCZ <bobes.chaos@gmail.com>
2023-03-07 21:00:02 +0100

    fix pagination + name search

--- src/pages/search/components/name-tab/utils.ts
+++ src/pages/search/components/name-tab/utils.ts
@@ -1,5 +1,11 @@
 import { Game } from '../../../../shared/types';
 import { NameFilters } from './types';
 
+const normalizeString = (value: string) =>
+  value
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase();
+
 export const filterGameByName = (game: Game, { name }: NameFilters): boolean =>
-  !!name && game.sourceName.toLowerCase().includes(name.toLowerCase());
+  !!name && normalizeString(game.sourceName).includes(normalizeString(name));
